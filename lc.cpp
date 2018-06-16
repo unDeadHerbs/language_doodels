3843c881c19229cb69b92df1c6885be97d1e24fb
unDeadHerbs <undeadherbs@gmail.com>
2018-06-16 07:20:54 -0500

    passed all of the current tests

--- lc.cpp
+++ lc.cpp
@@ -35,25 +35,54 @@ int find_match(const string l, const int p) {
 }
 
 string brep(const string l, const string v, const int d= 0) {
+	// cout << "dbg l:" << l << " v:" << v << " d:<" << d << endl;
 	if (!l.size()) return l;
 	if (d == 0) return brep(l.substr(1), v, 1);
-	if (l[0] == 'l') return 'l' + brep(l.substr(1), v, d + 1);
-	if (l[0] == '0' + d) return v + brep(l.substr(1), v, d);
-	if (l[0] == ')') return ')' + brep(l.substr(1), v, d - 1);
+	switch (l[0]) {
+	case 'l':
+		return 'l' + brep(l.substr(1), v, d + 1);
+
+	case '1':
+	case '2':
+	case '3':
+	case '4':
+	case '5':
+	case '6':
+	case '7':
+	case '8':
+	case '9':
+		if (l[0] - '0' == d)
+			return v + brep(l.substr(1), v, d);
+		else
+			return l[0] + brep(l.substr(1), v, d);
+	case ')':
+		return ')' + brep(l.substr(1), v, d - 1);
+	}
 	return l[0] + brep(l.substr(1), v, d);
 }
 
 string b(const string l) {
 	if (l[0] == '(') {
 		const int f= find_match(l, 1);
-		if (l[1] == '(') { return "TODO: call b on sub and ret"; }
-		if (!(l.size() - f)) return l.substr(1, f - 1);
+		if (l[1] == '(') {
+			const int s= find_match(l, 2);
+			if (s + 1 == f)
+				return l.substr(1, s) + l.substr(f + 1);
+			else {
+				return '(' + b(l.substr(1, f - 1)) + ')'
+				       + l.substr(f + 1);
+			}
+		}
+		if (!(l.size() - f - 1)) return l.substr(1, f - 1);
 		if (l[f + 1] == '(') {
 			int s= find_match(l, f + 2);
-			return brep(l.substr(1, f - 1), l.substr(f + 1, s - 1))
-			       + l.substr(s + 1);
+			return '(' + brep(l.substr(1, f - 1),
+			                  l.substr(f + 1, s - f))
+			       + ')' + l.substr(s + 1);
 		} else {
-			return brep(l.substr(1, f - 1), l.substr(f + 1));
+			return '('
+			       + brep(l.substr(1, f - 1), l.substr(f + 1, 1))
+			       + ')' + l.substr(f + 2);
 		}
 	} else
 		return l;
