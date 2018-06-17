f30c1d74807ad2af68f55cf4901d8b0cdd50ab29
unDeadHerbs <undeadherbs@gmail.com>
2018-06-17 11:19:12 -0500

    beta reduction now understands higher indexes

--- lc.cpp
+++ lc.cpp
@@ -22,6 +22,17 @@ int find_match(const string l, const int p) {
  * Lambda-Calc beta reducer
  */
 
+string inc(const string l, const int v= 1, int d= 0) {
+	if (!l.size()) return l;
+	if (v > 1) return inc(inc(l), v - 1);
+	if (l[0] == 'l') return l[0] + inc(l.substr(1), v, d + 1);
+	if (l[0] == ')') return l[0] + inc(l.substr(1), v, d - 1);
+	if (l[0] >= '1' && l[0] <= '9')
+		if (l[0] - '0' > d)
+			return char(l[0] + 1) + inc(l.substr(1), v, d);
+	return l[0] + inc(l.substr(1), v, d);
+}
+
 string brep(const string l, const string v, const int d= 0) {
 	if (!l.size()) return l;
 	if (d == 0) return brep(l.substr(1), v, 1);
@@ -39,9 +50,11 @@ string brep(const string l, const string v, const int d= 0) {
 	case '8':
 	case '9':
 		if (l[0] - '0' == d)
-			return v + brep(l.substr(1), v, d);
-		else
+			return inc(v, d - 1) + brep(l.substr(1), v, d);
+		else if (l[0] - '0' < d)
 			return l[0] + brep(l.substr(1), v, d);
+		else
+			return char(l[0] - 1) + brep(l.substr(1), v, d);
 	case ')':
 		return ')' + brep(l.substr(1), v, d - 1);
 	}
